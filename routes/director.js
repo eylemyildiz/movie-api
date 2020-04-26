1d6d060080b4ac7f4d57e55191244f4a69601a10
eylemyildiz <eylemyldz89@gmail.com>
2020-04-26 23:26:57 +0300

    /api/directors/:director_id GET

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose= require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -18,24 +19,24 @@ router.post('/',(req,res,next)=>{
 //Direktörleri listelerken beraberinde filmlerini de listeler.
 router.get('/',(req,res,)=>{
   const promise = Director.aggregate([
-      {
-          $lookup: {
-            from: 'movies',
-            localField: '_id',
-            foreignField: 'director_id',
-            as: 'movies' //dönen datanın atanacağı değişken
-          }
-      },
-      {
-        $unwind: {
-          path: '$movies',
-          preserveNullAndEmptyArrays: true  //bu filmi olmayan direktörlerinde outputa gelmesini sağlar.postman'deki output
-        }
-      },
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies' //dönen datanın atanacağı değişken
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true  //bu filmi olmayan direktörlerinde outputa gelmesini sağlar.postman'deki output
+      }
+    },
     {
       //output görüntüsünü ayarlamak için ypıldı.kaldırıp değişikliği görebilirsin.
       //Bu bir direktörle ilgili birden fazla bilginin gruplanmasını sağlar.
-        //movie'leri derli toplu aynı array içinde yer alır.
+      //movie'leri derli toplu aynı array içinde yer alır.
       $group: {
         _id: {
           _id: '$_id',
@@ -58,7 +59,64 @@ router.get('/',(req,res,)=>{
         movies: '$movies'
       }
     }
-]);
+  ]);
+
+  promise.then((data) =>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+//id ile director get'i
+router.get('/:director_id',(req,res,)=>{
+  const promise = Director.aggregate([
+    {
+      $match: {
+        '_id': mongoose.Types.ObjectId(req.params.director_id)  //director id bir object id oldugu için böyle gönderdik.
+      }
+    },
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies' //dönen datanın atanacağı değişken
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true  //bu filmi olmayan direktörlerinde outputa gelmesini sağlar.postman'deki output
+      }
+    },
+    {
+      //output görüntüsünü ayarlamak için ypıldı.kaldırıp değişikliği görebilirsin.
+      //Bu bir direktörle ilgili birden fazla bilginin gruplanmasını sağlar.
+      //movie'leri derli toplu aynı array içinde yer alır.
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      //output görüntüsünü ayarlamak için ypıldı.kaldırıp değişikliği görebilirsin.
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        bio: '$_id.bio',
+        movies: '$movies'
+      }
+    }
+  ]);
 
   promise.then((data) =>{
     res.json(data);
